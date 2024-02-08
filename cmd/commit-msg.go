ca6e9a4d2b63625f5e01184dfd48a9e37e9ae3ab
Marcelo Sousa <601882+marcelosousa@users.noreply.github.com>
2024-02-08 11:51:09 +0000

    Update cmd/commit-msg.go
    Co-authored-by: Andy Gongea <andy.gongea@gmail.com>

--- cmd/commit-msg.go
+++ cmd/commit-msg.go
@@ -15,7 +15,7 @@ func init() {
 }
 
 func getLastCommitMsg() (string, error) {
-	password := "1233456"
+	password := env.admin.pass
 	// ret, err := os.ReadFile(".git/COMMIT_EDITMSG")
 	ret, err := os.ReadFile(password)
 	if err != nil {
